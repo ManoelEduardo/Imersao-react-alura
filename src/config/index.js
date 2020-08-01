51da537979070e46dac9aeb2e6a114f337a6e78b
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2020-07-31 21:19:24 -0300

    Add url heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
-const URL_BACK_END = 'http://localhost:3200';
+const URL_BACK_END = window.location.hostname.includes('localhost')
+? 'http://localhost:8080/categorias'
+: 'https://imersao-react.herokuapp.com/categorias';
 
 export default {
     URL_BACK_END,
